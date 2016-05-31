486e4aa70083b7cfa7c9fb53918eba8a8e524d2d
arghtype <arghtype@gmail.com>
2016-05-31 21:18:51 +0400

    Initial commit

---
+++ src/main/java/argh/ds3saveparser/Main.java
@@ -0,0 +1,12 @@
+package argh.ds3saveparser;
+
+/**
+ * @author arghtype@gmail.com
+ * @since 31.05.2016
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
